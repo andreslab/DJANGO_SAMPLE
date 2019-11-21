081a3199f34c09a3fb4df9ce065e78e98f84d039
Jaime Andrade <jaimeandrade454@gmail.com>
2019-11-21 07:51:51 -0500

    save 21-11-19

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -4,4 +4,11 @@ from .views import crearAutor, listarAutor
 urlpatterns = [
    path('crear_autor/', crearAutor, name = 'crear_autor'),
    path('lista_autores/', listarAutor, name = 'listar_autores')
+
+   #pasar parametros
+   #path('lista_autores/<slug:titulo>', listarAutor, name = 'listar_autores') 
+   # #estoy pasando un string con slug
+
+   #usar re_path para agregar expresiones regulares
+   #re_path(r'^crear_autor/(?P<id>\d)', listarAutor, name = 'listar_autores' )
 ]
